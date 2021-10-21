f88b5dd85bfe119f9d772f599d0a65394c7e3e5e
Dan Savin <a8ton1@gmail.com>
2021-10-21 03:15:13 +0300

    Импорт исключение для вывода адекватного сообщения об ошибке

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,7 +1,19 @@
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 class BasePage():
     def __init__(self, browser, url):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(10)
 
     def open(self):
         self.browser.get(self.url)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except (NoSuchElementException):
+            return False
+        return True
--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,11 @@
 from pages.main_page import MainPage
 
+def test_guest_should_be_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    page.open()                      # открываем страницу
+    page.should_be_login_link()
+
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
