58db3e9e90bc467071bf48ef58f2e1c4b1c906c7
Dan Savin <a8ton1@gmail.com>
2021-10-21 14:35:34 +0300

    Сравнение цены и названия в корзине и магазине и xfail багнутой ссылки

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,11 @@ class LoginPageLocators():
     LOGIN_URL = (By.CSS_SELECTOR, "#login_link")
     REG_FORM = (By.CSS_SELECTOR, "#register_form")
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+
+class ProductPageLocators():
+    ADD_BUTTON = (By.CSS_SELECTOR, "#add_to_basket_form")
+    NAME_PRODUCT_IN_BASKET = (By.CSS_SELECTOR, "div:nth-child(1) > .alertinner > strong")
+    PRICE_PRODUCT_IN_BASKET = (By.CSS_SELECTOR, "div:nth-child(3) > .alertinner > p > strong")
+    NAME_PRODUCT = (By.CSS_SELECTOR, ".product_main > h1")
+    PRICE_PRODUCT = (By.CSS_SELECTOR, "p.price_color")
+    
---
+++ pages/product_page.py
@@ -0,0 +1,19 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+from selenium.webdriver.common.by import By
+import time
+
+class ProductPage(BasePage): 
+    def add_to_basket(self):
+        assert self.is_element_present(*ProductPageLocators.ADD_BUTTON), "Button is not presented"
+        button = self.browser.find_element(*ProductPageLocators.ADD_BUTTON).click()
+
+    def name_product_in_basket(self):
+        name_basket = self.browser.find_element(*ProductPageLocators.NAME_PRODUCT_IN_BASKET).text
+        name = self.browser.find_element(*ProductPageLocators.NAME_PRODUCT).text
+        assert name_basket == name, "Name is incorrect"
+        
+    def price_product_in_basket(self):
+        price_basket = self.browser.find_element(*ProductPageLocators.PRICE_PRODUCT_IN_BASKET).text
+        price = self.browser.find_element(*ProductPageLocators.PRICE_PRODUCT).text
+        assert price_basket == price, "Price is incorrect"
